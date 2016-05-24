c3fb506c79e09745a18551c8efe1f10300396df0
RobinBKing <samimspop@gmail.com>
2016-05-23 22:24:36 -0700

    add side nav

--- js/init.js
+++ js/init.js
@@ -1,7 +1,19 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+        // menuWidth: 240, // Default is 240
+        // edge: 'left', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
+
+    $('.button-collapse-about').sideNav({
+        menuWidth: 275, // Default is 240
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
     $('.parallax').parallax();
 
   }); // end of document ready
